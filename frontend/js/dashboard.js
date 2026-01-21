aa0d6f58a70c82d26745cf6844a7b4e8fae90a02
Prakhar Singhal <prakharsinghal@Prakhars-MacBook-Air-2.local>
2026-01-21 23:12:54 +0530

    Commit

---
+++ frontend/js/dashboard.js
@@ -0,0 +1,41 @@
+// frontend/js/dashboard.js
+
+async function loadDashboard() {
+    try {
+        const user = await apiRequest("/users/me");
+
+        document.getElementById("welcome").textContent =
+            `Welcome ${user.userid} (${user.role})`;
+
+        if (user.role === "student") {
+            loadStudentView();
+        } else if (user.role === "recruiter") {
+            loadRecruiterView();
+        } else if (user.role === "admin") {
+            loadAdminView();
+        }
+
+    } catch (err) {
+        // token invalid or expired
+        localStorage.clear();
+        window.location.href = "index.html";
+    }
+}
+
+function loadStudentView() {
+    document.getElementById("content").textContent =
+        "Student dashboard coming next...";
+}
+
+function loadRecruiterView() {
+    document.getElementById("content").textContent =
+        "Recruiter dashboard coming next...";
+}
+
+function loadAdminView() {
+    document.getElementById("content").textContent =
+        "Admin dashboard coming next...";
+}
+
+loadDashboard();
+
